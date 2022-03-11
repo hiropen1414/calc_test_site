c08da94425aa225fd21b2c26652ea3c4dae5674e
hiro <hiropen1414@gmail.com>
2022-03-11 17:45:35 +0900

    aa

--- alert.js
+++ alert.js
@@ -1,19 +1,17 @@
 'use strict';
-const planet ="What's your name?";
+const planet ="please input two numbers with space";
 let result = prompt(planet);
-
-for(let i=0;i<50;i++){
-	let ret = urusai(i);
-	alert(ret);
-}
+let num[2]=result.split(" ");
+let ret = grtLower(num);
+alert(`smaller is ${ret}`)
 
 
-
-
-function urusai(i){
-	if(i == 0){
-		return (1);
+function getLower(num){
+	if(+num[0] < +num[1]){
+		return num[0];
+	}else if(+num[0] > +num[1]){
+		return num[1];
 	}else{
-		return (i * urusai(i - 1));
+		return Nane. Same.;
 	}
 }
